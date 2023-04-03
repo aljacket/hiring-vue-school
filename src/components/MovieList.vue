3cba3f4e750189a59f4cc05696673a81d37dff9b
a.cavalieri <a.cavalieri@summonpress.com>
2023-04-03 21:16:42 +0200

    search bar

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,27 +1,39 @@
 <template>
 	<div>
-		<h2>Movies</h2>
-		<ul>
+		<h2 class="text-center font-bold">Movies</h2>
+		<div class="my-4 flex flex-col space-x-1 items-center justify-center">
+			<label for="searchTerm">Search movies:</label>
+			<input
+				class="border border-gray-300 rounded-md px-2 py-1"
+				type="text"
+				id="searchTerm"
+				v-model="searchTerm"
+				@input="searchMovies"
+			/>
+		</div>
+		<ul v-if="hasMovies">
 			<li v-for="movie in movies" :key="movie.imdbID">
 				{{ movie.Title }} ({{ movie.Year }})
 			</li>
 		</ul>
+		<p v-else>No movies found.</p>
 	</div>
 </template>
 
 <script setup>
 	import axios from 'axios'
-	import { ref, onMounted } from 'vue'
+	import { ref, computed } from 'vue'
 
 	const movies = ref([])
+	const searchTerm = ref('')
 
-	const getMovies = async () => {
-		const apiKey = '8679ba82'
+	const hasMovies = computed(() => movies.value?.length > 0)
+
+	const searchMovies = async () => {
+		const apiKey = import.meta.env.VITE_OMD_API
 		const response = await axios.get(
-			`https://www.omdbapi.com/?apikey=${apiKey}&s=Star%20Wars`
+			`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm.value}`
 		)
 		movies.value = response.data.Search
 	}
-
-	onMounted(getMovies)
 </script>
